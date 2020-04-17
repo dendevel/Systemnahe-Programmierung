02a847142819421ee2b2ba218d8763f15a36b60b
Denis Dengler <denglerdenis@yahoo.de>
2020-04-17 12:50:58 +0200

    initial commit

---
+++ 6BitLauflicht/6BitLauflicht/main.c
@@ -0,0 +1,20 @@
+/*
+ * 6BitLauflicht.c
+ *
+ * Created: 17.04.2020 12:45:59
+ * Author : Denis Dengler, Maximilian Kürschner, David Schader
+ * Version: 0.0.1
+ * Description: program to light up several count of LEDs in a row successively. If end reached, direction is changed.
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
