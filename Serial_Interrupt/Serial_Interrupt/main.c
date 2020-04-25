b578c6da7ea613e1badaeade214dd4912f78acb0
Denis Dengler <denglerdenis@yahoo.de>
2020-04-25 12:44:26 +0200

    added lib

--- Serial_Interrupt/Serial_Interrupt/main.c
+++ Serial_Interrupt/Serial_Interrupt/main.c
@@ -12,37 +12,25 @@
 #include <stdint.h>                     // needed for uint8_t
 
 #include <avr/interrupt.h>
-
+#include "serial.h"
 
 
 #define FOSC 16000000                       // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD -1
 
+void func(){
+	usart_send(usart_receive());
+}
 
-volatile char ReceivedChar;
 
 
 int main( void )
 {
-	/*Set baud rate */
-	UBRR0H = (MYUBRR >> 8);
-	UBRR0L = MYUBRR;
-	
-	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);      // Enable receiver and transmitter
-	UCSR0B |= (1 << RXCIE0);                    // Enable reciever interrupt
-	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);    // Set frame: 8data, 1 stp
-
-	sei();                                      // Lets not forget to enable interrupts =P
-
+	usart_init(func);
+	usart_send_string("hallo_test\n\r");
 	while(1)
-	{
-		;                                      // Main loop
+	{                              // Main loop
 	}
 }
 
-ISR (USART_RX_vect)
-{
-	ReceivedChar = UDR0;                       // Read data from the RX buffer
-	UDR0 = ReceivedChar;                       // Write the data to the TX buffer
-}
--- Serial_Interrupt/Serial_Interrupt/serial.h
+++ Serial_Interrupt/Serial_Interrupt/serial.h
@@ -9,7 +9,15 @@
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-void usart_init();
+#include <avr/io.h>
+#include <stdint.h>                     // needed for uint8_t
+#include <avr/interrupt.h>
+
+#define FOSC 16000000                       // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD -1
+
+void usart_init(void (*fun)());
 void usart_send(uint8_t toSend);
 void usart_send_string(char *data);
 uint8_t usart_receive();
