0254f47779f91a0587bd84e00909d15c6d05de57
Denis <52169970+sheldon297@users.noreply.github.com>
2020-04-25 14:46:35 +0200

    Update main.c

--- ReadAnalog/ReadAnalog/main.c
+++ ReadAnalog/ReadAnalog/main.c
@@ -2,23 +2,20 @@
  * ReadAnalog.c
  *
  * Created: 25.04.2020 13:06:48
- * Author : dengl
+ * Author : Denis Dengler, Maximilian Kürschner, David Schader
+ * Description: Read value from potentiometer and print in serial monitor
  */ 
 
 #include <avr/io.h>
 #include "serial.h"
 
-// this code continually scans ADC0 for an analog signal, using 16Mhz processor clock
-
-
-#include <avr/io.h>
 #include <stdlib.h>
-#include <stdint.h>       // needed for uint8_t
+#include <stdint.h>       		// needed for uint8_t
 
 #include <avr/interrupt.h>
 
 
-volatile uint8_t ADCvalue;    // Global variable, set to volatile if used with ISR
+volatile uint8_t ADCvalue;    		// Global variable, set to volatile if used with ISR
 
 void echo(){
 	usart_send(usart_receive());
@@ -45,6 +42,7 @@ int main(void)
 
 
 	usart_init(echo);
+	
 	while (1)
 	{
 		// main loop
