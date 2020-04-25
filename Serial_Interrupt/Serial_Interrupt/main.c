2f0f4c4159af3524dd9e3c1608bbb906e2e47e20
Denis <52169970+sheldon297@users.noreply.github.com>
2020-04-25 14:19:43 +0200

    Update main.c

--- Serial_Interrupt/Serial_Interrupt/main.c
+++ Serial_Interrupt/Serial_Interrupt/main.c
@@ -2,24 +2,13 @@
  * Serial_Interrupt.c
  *
  * Created: 25.04.2020 10:29:20
- * Author : dengl
+ * Author : Denis Dengler, Maximilian Kürschner, David Schader
+ * Description: Using serial pins for echo function
  */ 
 
-// This code waits for a character and transmits the character back (with interrupts)
-
-
-#include <avr/io.h>
-#include <stdint.h>                     // needed for uint8_t
-
-#include <avr/interrupt.h>
 #include "serial.h"
 
-
-#define FOSC 16000000                       // Clock Speed
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD -1
-
-void func(){
+void echo(){
 	usart_send(usart_receive());
 }
 
@@ -27,7 +16,8 @@ void func(){
 
 int main( void )
 {
-	usart_init(func);
+	usart_init(echo);
+	//test string
 	usart_send_string("hallo_test\n\r");
 	while(1)
 	{                              // Main loop
