a805b159ac33e357f59ab3a590c2430a425e372d
Denis <52169970+sheldon297@users.noreply.github.com>
2020-04-25 14:16:15 +0200

    Update main.c

--- Serial/Serial/main.c
+++ Serial/Serial/main.c
@@ -12,9 +12,9 @@
 #include <stdint.h>                     // needed for uint8_t
 
 
-#define FOSC 16000000                       // Clock Speed
+#define FOSC 16000000                   // Clock Speed
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD -1
+#define MYUBRR FOSC/16/BAUD -1		//Transmitting Speed
 
 
 char ReceivedChar;
@@ -32,10 +32,12 @@ int main( void )
 
 	while(1)
 	{
+		//The RXC(RXCn) bit (Receive Complete) will be set to HIGH(1) when data has been received and is available in the UDR (UDR0) buffer. 
 		while ( !(UCSR0A & (1 << RXC0)) ){}  // Wait until data is received
 		
 		ReceivedChar = UDR0;                    // Read the data from the RX buffer
 		
+		//The UDRE(UDREn) is set to HIGH(1) when the buffer (UDR/UDRn) is empty, and therefore ready to be written. 
 		while ( !(UCSR0A & (1 << UDRE0)) ){} // Wait until buffer is empty
 
 		UDR0 = ReceivedChar;                    // Send the data to the TX buffer
