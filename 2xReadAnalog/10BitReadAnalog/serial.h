989add5fa87db7637976cd11343e9f325ae021f8
DevedGames <47224024+DevedGames@users.noreply.github.com>
2020-04-25 19:17:10 +0200

    initial 2xReadAnalog

---
+++ 2xReadAnalog/10BitReadAnalog/serial.h
@@ -0,0 +1,26 @@
+/*
+ * serial.h
+ *
+ * Created: 25.04.2020 10:55:22
+ *  Author: dengl
+ */ 
+
+
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+#include <avr/io.h>
+#include <stdint.h>                     // needed for uint8_t
+#include <avr/interrupt.h>
+
+#define FOSC 16000000                       // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD -1
+
+void usart_init(void (*fun)());
+void usart_send(uint8_t toSend);
+void usart_send_string(char *data);
+uint8_t usart_receive();
+
+
+#endif /* SERIAL_H_ */
