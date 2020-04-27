8313a53f0eed13b7e18b21be4b170991dc823bdf
Denis Dengler <denglerdenis@yahoo.de>
2020-04-27 18:04:27 +0200

    code improvement

--- ReadAnalog/ReadAnalog/serial.h
+++ ReadAnalog/ReadAnalog/serial.h
@@ -2,7 +2,7 @@
  * serial.h
  *
  * Created: 25.04.2020 10:55:22
- *  Author: Denis Denlger, Maximilian Kürschner, David Schader
+ *  Author: Denis Denlger, Maximilian KÃ¼rschner, David Schader
  */ 
 
 
@@ -11,18 +11,18 @@
 
 /* includes */
 #include <avr/io.h>
-#include <stdint.h>							// needed for uint8_t
+#include <stdint.h>							            // needed for uint8_t
 #include <avr/interrupt.h>
 
 /* constants and typedefs */
 #define FOSC 16000000                       // Clock Speed
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD -1				// Transmittin Speed
+#define MYUBRR FOSC/16/BAUD -1				      // Transmittin Speed
 
 typedef void (*func)(void);
 
 /* function prototypes */
-void usart_init(void (*fun)());
+void usart_init(func);
 void usart_send(uint8_t toSend);
 void usart_send_string(char *data);
 uint8_t usart_receive();
